63f964f477cca6e379ffd9b5faada66f13ff590b
RUK KEOSUNG <rukzeedza@gmail.com>
2025-05-10 15:02:35 +0700

    build: install shadcn ui instead of chakra ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Provider } from "@/components/ui/provider"
 
 
 const geistSans = Geist({
@@ -27,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <Provider>{children}</Provider>
+        {children}
       </body>
     </html>
   );
