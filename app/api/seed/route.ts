3154aea430cbae74ff168ac39576b11c9807ee59
RUK KEOSUNG <rukzeedza@gmail.com>
2025-05-17 12:35:04 +0700

    style: formate code using Prettier

--- app/api/seed/route.ts
+++ app/api/seed/route.ts
@@ -1,15 +1,18 @@
-import { reseedAttachmentsTable, reseedPlansTable, reseedUsersTable } from "@/lib/actions";
+import {
+  reseedAttachmentsTable,
+  reseedPlansTable,
+  reseedUsersTable,
+} from "@/lib/actions";
 import { NextResponse, NextRequest } from "next/server";
 
 export const POST = async (req: NextRequest) => {
+  try {
+    await reseedUsersTable();
+    await reseedPlansTable();
+    await reseedAttachmentsTable();
 
-    try {
-        await reseedUsersTable()
-        await reseedPlansTable()
-        await reseedAttachmentsTable()
-
-        return NextResponse.json({ message: "sucessfully" }, { status: 200 })
-    } catch (error) {
-        return NextResponse.json({ api: error }, { status: 500 })
-    }
-}
+    return NextResponse.json({ message: "sucessfully" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ api: error }, { status: 500 });
+  }
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,62 +1,76 @@
-"use client"
+"use client";
 
 import LeftNavbar from "@/components/leftnav/LeftNavbar";
 import BaseMapContent from "@/components/map/BaseMapContainer";
 import CreatePlan from "@/components/modal/CreatePlan";
 import RightNavbar from "@/components/rightnav/RightNavbar";
 import { useState, createContext, useEffect } from "react";
-import { TypeMapContext } from "@/types/context"
-import { PlanForm } from "@/types/base"
+import { TypeMapContext } from "@/types/context";
+import { PlanForm } from "@/types/base";
 import axios from "axios";
 import { basemaps } from "@/data/basemaps";
 
-
 export const MapContext = createContext<TypeMapContext>({
   feature: null,
-  setFeature: () => { },
+  setFeature: () => {},
   isPlanCreated: false,
-  setIsPlanCreated: () => { },
+  setIsPlanCreated: () => {},
   selectedData: null,
-  setSelectedData: () => { },
+  setSelectedData: () => {},
   isDeleted: false,
-  setIsDeleted: () => { },
+  setIsDeleted: () => {},
   isCreateMapActive: false,
-  setIsCreateMapActive: () => { },
+  setIsCreateMapActive: () => {},
   isCancel: false,
-  setIsCancel: () => { },
+  setIsCancel: () => {},
   selectBaseMap: null,
-  setSelectBaseMap: () => { },
-})
+  setSelectBaseMap: () => {},
+});
 
 export default function Home() {
-  const [feature, setFeature] = useState<PlanForm | null>(null)
-  const [isPlanCreated, setIsPlanCreated] = useState(false)
-  const [planList, setPlanList] = useState(null)
-  const [selectedData, setSelectedData] = useState(null)
-  const [isDeleted, setIsDeleted] = useState(false)
-  const [isCreateMapActive, setIsCreateMapActive] = useState(false)
-  const [isCancel, setIsCancel] = useState(false)
-  const [selectBaseMap, setSelectBaseMap] = useState(null)
-
+  const [feature, setFeature] = useState<PlanForm | null>(null);
+  const [isPlanCreated, setIsPlanCreated] = useState(false);
+  const [planList, setPlanList] = useState(null);
+  const [selectedData, setSelectedData] = useState(null);
+  const [isDeleted, setIsDeleted] = useState(false);
+  const [isCreateMapActive, setIsCreateMapActive] = useState(false);
+  const [isCancel, setIsCancel] = useState(false);
+  const [selectBaseMap, setSelectBaseMap] = useState(null);
 
   useEffect(() => {
     const fetchPlans = async () => {
       try {
-        const response = await axios.get(`/api/getallplan`)
-        const data = response.data.response
-        setPlanList(data)
+        const response = await axios.get(`/api/getallplan`);
+        const data = response.data.response;
+        setPlanList(data);
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
-    }
-    fetchPlans()
+    };
+    fetchPlans();
 
-    setSelectBaseMap(basemaps[0].url)
-  }, [feature, isDeleted])
-
+    setSelectBaseMap(basemaps[0].url);
+  }, [feature, isDeleted]);
 
   return (
-    <MapContext value={{ feature, setFeature, isPlanCreated, setIsPlanCreated, selectedData, setSelectedData, isDeleted, setIsDeleted, isCreateMapActive, setIsCreateMapActive, isCancel, setIsCancel, selectBaseMap, setSelectBaseMap }}>
+    <MapContext
+      value={{
+        feature,
+        setFeature,
+        isPlanCreated,
+        setIsPlanCreated,
+        selectedData,
+        setSelectedData,
+        isDeleted,
+        setIsDeleted,
+        isCreateMapActive,
+        setIsCreateMapActive,
+        isCancel,
+        setIsCancel,
+        selectBaseMap,
+        setSelectBaseMap,
+      }}
+    >
       <main className="flex">
         <LeftNavbar data={planList} />
         <div className="w-screen h-screen relative">
